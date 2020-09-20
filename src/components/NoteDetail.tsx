9828a097465cfddc0f3c7d238d8fa68e3782c506
Jan-Kuta <jan.kuta@email.cz>
2020-09-20 20:53:49 +0200

    Pridana knihovna Material ui Zacatek prace na UI

--- src/components/NoteDetail.tsx
+++ src/components/NoteDetail.tsx
@@ -1,10 +1,72 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
+import React, {useEffect} from 'react';
+import {withRouter, RouteComponentProps} from 'react-router-dom';
+import {Button} from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
+import GoBackIcon from '@material-ui/icons/ArrowBack';
+import EditIcon from '@material-ui/icons/Edit';
+import {useDispatch, useSelector} from 'react-redux';
+import {goBack, push} from 'redux-first-history';
+import {RootState} from '../redux/rootReducer';
+import makeStyles from '@material-ui/core/styles/makeStyles';
 
-export const NoteDetail = () => (
-    <>
-        <NavLink to="/1/edit">Edit</NavLink>&nbsp;
-        <NavLink to="/">Home</NavLink>
-        <h1>NOTE DETAIL</h1>
-    </>
-);
+type MatchParams = {
+    id: string
+}
+
+type Props = RouteComponentProps<MatchParams>
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        maxWidth: '36ch',
+        backgroundColor: theme.palette.background.paper,
+    },
+    inline: {
+        display: 'inline',
+    },
+}));
+
+const PureNoteDetail = ({ match }: Props) => {
+    const dispatch = useDispatch();
+    const note = useSelector((state: RootState) => state.notes.data.find((note) => note.id.toString() === match.params.id));
+    useEffect(() => {
+        if(!note) {
+            dispatch(goBack('/'))
+        }
+    }, [dispatch, note]);
+    const classes = useStyles();
+
+    if (! note) {
+        return null;
+    }
+
+    return (
+        <>
+            <Card className={classes.root}>
+                <CardHeader
+                    action={
+                        <IconButton onClick={() => dispatch(goBack('/'))} aria-label="settings">
+                            <GoBackIcon/>
+                        </IconButton>
+                    }
+                    title="NOTE DETAIL"
+                />
+                <CardContent>
+                    Id: {note.id}
+                    Title: {note.title}
+                </CardContent>
+                <CardActions>
+                    <Button startIcon={<DeleteIcon />} variant="contained" color="secondary">Delete</Button>
+                    <Button onClick={()=> dispatch(push(`/${match.params.id}/edit`))} startIcon={<EditIcon />} variant="outlined" color="primary">Edit</Button>
+                </CardActions>
+            </Card>
+        </>
+    );
+};
+
+export const NoteDetail = withRouter(PureNoteDetail);
--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -5,6 +5,24 @@ import {useDispatch, useSelector} from 'react-redux';
 import {loadNotes} from '../redux/notes/notes.actions';
 import {RootState} from '../redux/rootReducer';
 import {NoteCreator} from './NoteCreator';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        maxWidth: '36ch',
+        backgroundColor: theme.palette.background.paper,
+    },
+    inline: {
+        display: 'inline',
+    },
+}));
 
 export const NoteList = () => {
     const dispatch =  useDispatch();
@@ -15,15 +33,25 @@ export const NoteList = () => {
             dispatch((loadNotes()))
         }
     }, [dispatch, notes]);
+    const classes = useStyles();
 
     return(
         <>
             <h1>NOTE LIST</h1>
-            <ul>
+            <List className={classes.root}>
                 {notes.map((note => (
-                    <li key={note.id} onClick={() => dispatch(push(`/${note.id}`))}>{note.title}</li>
+                    <ListItem key={note.id}>
+                        <ListItemText
+                            primary={note.title}
+                        />
+                        <ListItemSecondaryAction>
+                            <IconButton onClick={() => dispatch(push(`/${note.id}`))} edge="end" aria-label="delete">
+                                <MoreIcon/>
+                            </IconButton>
+                        </ListItemSecondaryAction>
+                    </ListItem>
                 )))}
-            </ul>
+            </List>
             <NoteCreator />
         </>
     );
